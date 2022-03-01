4e1b2c197164574a27456e3e87da3c857c0bdded
Kalyani Unnikrishnan <kalyaniunnikrishnan@Kalyanis-MacBook-Pro.local>
2022-02-28 21:03:25 -0600

    same

--- client.cc
+++ client.cc
@@ -98,7 +98,7 @@ static int client_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 			 off_t offset, struct fuse_file_info *fi, enum fuse_readdir_flags flags)
 {
 
-    return options.afsclient->ListDir(path, buf, filler);
+    return options.afsclient->afs_LS(path, buf, filler);
     
 }
 
