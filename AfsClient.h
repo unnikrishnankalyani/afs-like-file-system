96205b8c6dd9e18b11219887b91c5760b7a950f5
Monish Shivappa <monshiva@Monishs-Air.lan>
2022-03-01 23:26:20 -0600

    reverting all changes

--- AfsClient.h
+++ AfsClient.h
@@ -56,17 +56,18 @@ class AfsClient {
         FetchRequest request;
         request.set_path(path);
 
-        FetchReply reply;
+
+        FetchReply *reply = new FetchReply();
 
         ClientContext context;
 
-        Status status = stub_->afs_FETCH(&context, request, &reply);
+        Status status = stub_->afs_FETCH(&context, request, reply);
 
         if (status.ok()) {
-                    std::cout << (*reply)->buf() <<std::endl;
-            *buf = (char *)((*reply)->buf()).data();
+                    std::cout << reply->buf() <<std::endl;
+            *buf = (char *)(reply->buf()).data();
                     printf("%s\n", *buf);
-            *size = (*reply)->size();
+            *size = reply->size();
             return 0;
         } else {
             return -1;
