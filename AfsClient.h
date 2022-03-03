f91ac37e328038531a511e1c3ec18f072d41ac82
Kalyani Unnikrishnan <kalyaniunnikrishnan@Kalyanis-MacBook-Pro.local>
2022-03-02 19:11:56 -0600

    --debug

--- AfsClient.h
+++ AfsClient.h
@@ -85,13 +85,8 @@ class AfsClient {
             return -1;
         }
     }
-<<<<<<< HEAD
-    
-    int afs_OPEN(const char *path, struct fuse_file_info *file_info, char fs_path[])
-=======
 
     int afs_OPEN(const char *path, struct fuse_file_info *file_info, char cache_path[])
->>>>>>> 93de2d8 (--debug)
     {
             char *buf;
             int size;
@@ -311,18 +306,8 @@ class AfsClient {
         if(ret_code < 0) {
             printf("Error while writing into file: %d\n", errno);
             int fd;
-            char cached_file[80];
-            char local_path[PATH_MAX];
-            local_path[0] = '\0';
-
-<<<<<<< HEAD
-            snprintf(cached_file, 80, "%lu", hash((unsigned char *)path));
-            strncat(local_path, fs_path, PATH_MAX);
-=======
-            // snprintf(cached_file, 80, "%lu", hash((unsigned char *)path));
-            strncat(local_path, cache_path, PATH_MAX);
->>>>>>> 93de2d8 (--debug)
-            strncat(local_path, cached_file, PATH_MAX);
+            char local_path[MAX_PATH_LENGTH];
+            getLocalPath(path, cache_path, client_path);
 
             fd = open(local_path,  O_APPEND | O_RDWR);
 
