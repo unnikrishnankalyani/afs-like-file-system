af835c1159bbc65ea648971d8f57f12f337160a5
Kalyani Unnikrishnan <kalyaniunnikrishnan@Kalyanis-MacBook-Pro.local>
2022-03-02 20:33:38 -0600

    debug

--- client.cc
+++ client.cc
@@ -96,6 +96,7 @@ static int client_read(const char *path, char *buffer, size_t size, off_t offset
 static int client_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 			 off_t offset, struct fuse_file_info *fi, enum fuse_readdir_flags flags)
 {
+    std::cout <<"readdir " <<std::endl;
     return options.afsclient->afs_LS(path, buf, filler);
 }
 
