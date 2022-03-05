5581f16c7e7052f95db47810a13925aea3d6cbba
Kalyani Unnikrishnan <kalyaniunnikrishnan@Kalyanis-MacBook-Pro.local>
2022-03-05 15:32:56 -0600

    prints

--- AfsClient.h
+++ AfsClient.h
@@ -491,15 +491,15 @@ class AfsClient {
             if (ret_code == -1)
                 ret_code = -errno;
             struct stat info;
-            lstat(client_path, &info);
+            stat(client_path, &info);
             printf("~~~~~~~client_path: Last Mod: %ld\n", info.st_mtime);
-            lstat(client_tmp_path, &info);
+            stat(client_tmp_path, &info);
             printf("~~~~~~~client_tmp_path: Last Mod: %ld\n", info.st_mtime);
             close(file_info->fh);
             close(fd);
             remove(client_path);
             rename(client_tmp_path, client_path);
-            lstat(client_path, &info);
+            stat(client_path, &info);
             printf("~~~~~~~client_path: Last Mod: %ld\n", info.st_mtime);
             file_info->fh = open(client_path, O_RDWR | O_CREAT | O_TRUNC, S_IRWXU | S_IRWXG);
         }
