a1f83667a98a2313164c24158a62c9137a33da13
Kalyani Unnikrishnan <kalyaniunnikrishnan@Kalyanis-MacBook-Pro.local>
2022-03-05 21:33:28 -0600

    prints

--- commonheaders.h
+++ commonheaders.h
@@ -188,7 +188,7 @@ printf("path: %s\n", client_path);
   int i;
 
   for (i=0; i<HTABLESIZE; i++){
-      printf(i);
+      printf("i %d" , i);
     if(ht[i] != NULL){
       hash_node *temp_hn = ht[i];
       while (temp_hn!=NULL){
